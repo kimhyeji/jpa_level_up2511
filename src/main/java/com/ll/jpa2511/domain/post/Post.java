0ca5bf61a7bde5f64b9eae12796edba9c015efc2
kimhyeji <gpwl5589@gmail.com>
2025-11-03 14:29:23 +0900

    base code

---
+++ src/main/java/com/ll/jpa2511/domain/post/Post.java
@@ -0,0 +1,24 @@
+package com.ll.jpa2511.domain.post;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    private String username;
+    private String subject;
+    private String content;
+}

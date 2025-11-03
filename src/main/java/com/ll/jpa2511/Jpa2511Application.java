d24ec89af427ce8dc3b771c3d056d918c4504d4a
kimhyeji <gpwl5589@gmail.com>
2025-11-03 14:13:13 +0900

    init

---
+++ src/main/java/com/ll/jpa2511/Jpa2511Application.java
@@ -0,0 +1,13 @@
+package com.ll.jpa2511;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jpa2511Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jpa2511Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/jpa2511/Jpa2511ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.jpa2511;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jpa2511ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
